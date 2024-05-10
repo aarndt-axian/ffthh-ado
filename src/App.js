805af47c5c3fd9e2aaa646d5711dc23ba43252db
Adam Arndt <85630678+aarndt-axian@users.noreply.github.com>
2024-05-10 16:31:53 -0700

    test CI change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn REEEEEEEEEEact
         </a>
       </header>
     </div>
